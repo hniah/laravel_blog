52a314797b7c546a3294049fcfcb067a66957f83
Hai Hoang <hoangngochai@gmail.com>
2017-01-20 21:30:54 +0700

    - Copy font to public/font

--- gulpfile.js
+++ gulpfile.js
@@ -3,29 +3,16 @@ const elixir = require('laravel-elixir');
 
 require('laravel-elixir-vue-2');
 
-// var paths = {
-//     'jquery': './vendor/bower_components/jquery/',
-//     'bootstrap': './vendor/bower_components/bootstrap-sass/assets/'
-// }
-
-var bower_path = "./vendor/bower_components";
 var paths = {
-  'jquery'     : bower_path + "/jquery/dist",
-  'bootstrap'  : bower_path + "/bootstrap-sass/assets",
-};
-
-/*mix.sass("app.scss", "public/assets/css", {
-  includePaths: [
-    paths.bootstrap + '/stylesheets',
-    paths.fontawesome + '/scss'
-  ]
-});*/
+    'jquery': './vendor/bower_components/jquery/',
+    'bootstrap': './vendor/bower_components/bootstrap-sass/assets/'
+}
 
 elixir(function(mix) {
-    mix.sass("app.scss", 'public/css/', null, {includePaths: [paths.bootstrap + '/stylesheets']})
+    mix.sass("app.scss", 'public/css/', null, {includePaths: [paths.bootstrap + 'stylesheets/']})
+        .copy(paths.bootstrap + 'fonts/bootstrap/**', 'public/fonts')
         .scripts([
-            paths.jquery + "/jquery.js",
-            paths.bootstrap + "/javascripts/bootstrap.js",
-        ],'public/js/app.js');
+            paths.jquery + "dist/jquery.js",
+            paths.bootstrap + "javascripts/bootstrap.js"
+        ], 'public/js/app.js');
 });
-
